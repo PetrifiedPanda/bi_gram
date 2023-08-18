19109ecd6c6b5afaa5c9f056e8da9e3ec0d51917
Benedikt Köhn <bfj.koehn@gmail.com>
2023-08-18 22:50:13 +0200

    Used split_whitespace instead of checking for empty strings

--- src/bi_gram.rs
+++ src/bi_gram.rs
@@ -45,7 +45,7 @@ struct FreqsAndOccurences<'a> {
 fn get_bi_gram_freqs(contents: &str) -> FreqsAndOccurences {
     let mut freqs = HashMap::<BiGram, u32>::new();
     let mut occurences = HashMap::<&str, u32>::new();
-    let mut it = contents.split(char::is_whitespace);
+    let mut it = contents.split_whitespace();
     let opt = it.next();
     let mut prev;
     match opt {
@@ -56,13 +56,7 @@ fn get_bi_gram_freqs(contents: &str) -> FreqsAndOccurences {
             return FreqsAndOccurences { freqs, occurences };
         }
     }
-    while prev.is_empty() {
-        prev = it.next().unwrap();
-    }
     for item in it {
-        if item.is_empty() {
-            continue;
-        }
         let bigram = BiGram {
             first: prev,
             second: item,
